7ba00795f95da6d6fe18d78d866b55ecc66893f9
Nivas Muthu M G <nivasmuthumg@icloud.com>
2021-06-30 22:04:28 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nivas Muthu M G on 30/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
